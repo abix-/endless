d96b251a09e81f7078e5b27b79c052ebc73142ef
abix <abix85@gmail.com>
2026-01-18 23:32:16 -0500

    camp icon 4x4 grid (~200px)

--- world/location.gd
+++ world/location.gd
@@ -17,6 +17,7 @@ const FRAMES := {
 }
 
 const LARGE_LOCATIONS := ["field"]  # Use 2x2 grid
+const XLARGE_LOCATIONS := ["camp"]  # Use 4x4 grid
 
 func _ready() -> void:
 	$Sprite2D.texture = preload("res://assets/roguelikeSheet_transparent.png")
@@ -26,7 +27,11 @@ func _ready() -> void:
 
 	if location_type in FRAMES:
 		var pos: Vector2i = FRAMES[location_type]
-		if location_type in LARGE_LOCATIONS:
+		if location_type in XLARGE_LOCATIONS:
+			# 4x4 grid of sprites (covers ~200px with 3x scale)
+			var size := SPRITE_SIZE * 4 + MARGIN * 3
+			$Sprite2D.region_rect = Rect2(pos.x * CELL, pos.y * CELL, size, size)
+		elif location_type in LARGE_LOCATIONS:
 			# 2x2 grid of sprites
 			var size := SPRITE_SIZE * 2 + MARGIN
 			$Sprite2D.region_rect = Rect2(pos.x * CELL, pos.y * CELL, size, size)
